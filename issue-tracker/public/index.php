51e6ae9e8f7ac4509fd36668bf8b1663d6e057c2
Kochelaev <qrbox@mail.ru>
2022-05-14 15:06:47 +0400

    first commit

---
+++ issue-tracker/public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Import the necessary classes
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+// Include the composer autoload file
+require '../vendor/autoload.php';
+
+// Setup a new Eloquent Capsule instance
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'issue-tracker-mysql',
+    'database'  => 'tracker',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+]);
+
+$capsule->bootEloquent();
+
+// Register a new user
+Sentinel::register([
+    'email'    => 'admin',
+    'password' => '123',
+]);
