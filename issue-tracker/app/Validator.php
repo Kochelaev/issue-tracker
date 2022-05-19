a27e18bf93ce82d2d8a7c56a0041d03bc869afed
Kochelaev <qrbox@mail.ru>
2022-05-19 23:49:43 +0400

    Class Validator added

---
+++ issue-tracker/app/Validator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Error;
+
+class Validator
+{
+    protected $request;
+
+    public function __construct(?array $request = null)
+    {
+        $this->request = $request ?: $_POST;
+    }
+
+    public function setRequset(array $request): void
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest(): ?array
+    {
+        return $this->request;
+    }
+
+    public function prepareRequst(): self
+    {
+        foreach ($this->request as &$req) {
+            $req = str_replace('`', '\\`', addslashes(nl2br(htmlspecialchars(trim($req)))));
+        }
+        return $this;
+    }
+
+    public static function CheckPostMethod(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST')
+            throw new \Error('нужен метод POST');
+    }
+}
